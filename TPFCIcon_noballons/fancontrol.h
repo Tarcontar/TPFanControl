03420afe7afc3f881e8db5bc342830d39f4f5a71
Stanislav Vinokurov <2657778+stenri@users.noreply.github.com>
2020-04-19 18:14:25 +0300

    Relicense project under GNU GPL v3 license (i.e. place a link at original public domain code, and license all my patches under GNU GPL v3).

--- TPFCIcon_noballons/fancontrol.h
+++ TPFCIcon_noballons/fancontrol.h
@@ -5,15 +5,25 @@
 //
 // --------------------------------------------------------------
 //
-//	This program and source code is in the public domain.
+// The original TPFanControl code is in public domain. See this fork for public domain code:
+// https://github.com/cjbdev/TPFanControl
+// All my own patches are licensed under GNU GPL v3 license. See notice below and LICENSE file for details.
 //
-//	The author claims no copyright, copyleft, license or
-//	whatsoever for the program itself (with exception of
-//	WinIO driver).  You may use, reuse or distribute it's 
-//	binaries or source code in any desired way or form,  
-//	Useage of binaries or source shall be entirely and 
-//	without exception at your own risk. 
+// Copyright (c) Stanislav Vinokurov, 2020
+//
+// This program is free software : you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+// GNU General Public License for more details.
 // 
+// You should have received a copy of the GNU General Public License
+// along with this program.If not, see < https://www.gnu.org/licenses/>.// 
+//
 // --------------------------------------------------------------
 
 #ifndef FANCONTROL_H
