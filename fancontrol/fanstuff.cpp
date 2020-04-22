08e8469b82783249c6772487b22ee7298a3c6d5f
trevnels <20nelson@da.org>
2020-04-22 19:01:58 -0400

    better logging of validation failures

--- fancontrol/fanstuff.cpp
+++ fancontrol/fanstuff.cpp
@@ -444,21 +444,25 @@ FANCONTROL::SetFan(const char *source, int fanctrl, BOOL final)
 
 			this->ReadByteFromEC(TP_ECOFFSET_FAN, &this->State.FanCtrl);
 
+			
+
 			if (this->State.FanCtrl != fanctrl2) {
+				sprintf_s(obuf + strlen(obuf), sizeof(obuf) - strlen(obuf), "(FAN2 WAS 0x%02x),", this->State.FanCtrl);
 				speedsvalid = 0;
 			}
 			
 			ok= this->WriteByteToEC(TP_ECOFFSET_FAN_SWITCH, TP_ECOFFSET_FAN1);
 
-			::Sleep(100);
+			::Sleep(300);
 
 			ok = this->ReadByteFromEC(TP_ECOFFSET_FAN, &this->State.FanCtrl);
 
 			if (this->State.FanCtrl != fanctrl) {
+				sprintf_s(obuf + strlen(obuf), sizeof(obuf) - strlen(obuf), "(FAN1 WAS 0x%02x),", this->State.FanCtrl);
 				speedsvalid = 0;
 			}
 
-			if(speedsvalid) break;
+			if(speedsvalid == 1) break;
 
 			::Sleep(300);
         }
